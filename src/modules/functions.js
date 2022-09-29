de8d44091dddc24c10b74b1ce2ef6c22d3894f2d
Solly <solobarine@gmail.com>
2022-09-30 00:41:15 +0100

    Fix Linters

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -3,13 +3,13 @@ import * as element from './elements.js';
 
 // Add Tasks to Array
 export const addTask = (x, y, z) => {
-  const obj = {a: x, b: y, c: z};
+  const obj = { a: x, b: y, c: z };
   taskList.push(obj);
 };
 
 // Load Tasks
 export const loadTask = (i) => {
-	element.taskContainer.innerHTML += `<div class="shell">
+  element.taskContainer.innerHTML += `<div class="shell">
                                      <input type="checkbox" class="checkbox"/>
                                      <p class="task-item">${taskList[i].a}</p>
                                      <button class="remBtn" type="button">Remove</button>
@@ -19,13 +19,13 @@ export const loadTask = (i) => {
 // Reload Tasks
 export const reloadTask = () => {
   element.taskContainer.innerHTML = '';
-  for (let i = 0; i < taskList.length; i++) {
+  for (let i = 0; i < taskList.length; i += 1) {
     element.taskContainer.innerHTML += `<div class="shell">
                                      <input type="checkbox" class="checkbox"/>
                                      <p class="task-item">${taskList[i].a}</p>
                                      <button class="remBtn" type="button">Remove</button>
                                      </div>`;
-	}
+  }
 };
 
 // Delete Task
@@ -35,7 +35,7 @@ export const deleteTask = (i) => {
 
 // Reload Task
 export const refreshIndex = () => {
- for (let i = 0; i < taskList.length; i += 1) {
+  for (let i = 0; i < taskList.length; i += 1) {
     taskList[i].c = i;
-  }	
+  }
 };
