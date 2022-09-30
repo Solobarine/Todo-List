07f4c2e164ae8cf0db74af6acf39fad7d238cba0
Solly <solobarine@gmail.com>
2022-09-30 18:20:46 +0100

    Add interactive functionality

--- src/index.js
+++ src/index.js
@@ -26,6 +26,36 @@ element.addBtn.addEventListener('click', () => {
   localStorage.setItem('listOfTasks', JSON.stringify(taskList));
 });
 
+// ------------------ Check the Box if b is set to true
+for (let i = 0; i < taskList.length; i++) {
+  if (taskList[i].b === true) {
+    element.taskContainer.children[i].children[0].checked = true;
+    element.taskContainer.children[i].classList.add('change-color');
+    element.taskContainer.children[i].children[1].style.textDecoration = 'line-through';
+  } else {
+    element.taskContainer.children[i].classList.remove('change-color');
+  }
+}
+
+//-----------------  Event Listener for CheckBox
+element.taskContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('checkbox')) {
+    const checkItem = e.target.parentElement;
+    const item = e.target.parentElement.children[0];
+    const checkIndex = taskList.findIndex((c) => c.a === checkItem.querySelector('.task-item').innerText);
+    taskList[checkIndex].b = item.checked;
+    if (item.checked === true) {
+      element.taskContainer.children[checkIndex].children[1].style.textDecoration = 'line-through';
+      checkItem.classList.add('change-color')
+      } else {
+        element.taskContainer.children[checkIndex].children[1].style.textDecoration = 'none';
+        checkItem.classList.remove('change-color')
+    }
+      localStorage.setItem('listOfTasks', JSON.stringify(taskList));
+      console.log(taskList);
+  }
+});
+
 // --------------------- Event Listener to Remove Button
 element.taskContainer.addEventListener('click', (e) => {
   if (e.target.classList.contains('remBtn')) {
@@ -36,4 +66,46 @@ element.taskContainer.addEventListener('click', (e) => {
     taskFunction.refreshIndex();
     localStorage.setItem('listOfTasks', JSON.stringify(taskList));
   }
+});
+
+// ---------------------- Edit To Do 
+element.taskContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('task-item')) {
+    const repParent = e.target.parentElement;
+    const repTask = e.target.parentElement.children[1];
+    const repInput = document.createElement('input');
+    const taskIndex = taskList.findIndex((t) => t.a === repParent.querySelector('.task-item').innerText);
+    repInput.className = 'repInput';
+    repInput.setAttribute('type', 'text');
+    repParent.replaceChild(repInput, repTask);
+    // -------------------- Collect User Input
+    repInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        const val = repInput.value;
+        // ------------------------ Replace The Old Task
+        taskList[taskIndex].a = val;
+        repParent.replaceChild(repTask, repInput);
+        repTask.textContent = val;
+        localStorage.setItem('listOfTasks', JSON.stringify(taskList));
+      }
+      localStorage.setItem('listOfTasks', JSON.stringify(taskList));
+    })
+  }
+});
+
+// ------------------- Function to clear all completed Tasks
+const clearAll = () => {
+  for (let i = 0; i < taskList.length; i += 1) {
+    if (taskList[i].b === true) {
+      taskList.splice(i, 1);
+      localStorage.setItem('listOfTasks', JSON.stringify(taskList));
+    }
+  }
+};
+
+// ------------------- Delete All Completed Tasks
+element.clearCompleted.addEventListener('click', () => {
+  clearAll();
+  taskFunction.reloadTask();
+  taskFunction.refreshIndex();
 });
